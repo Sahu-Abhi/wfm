e7d33242f399444a751119a37e97ede4ce928901
Sahu-Abhi <abhisahu162001@gmail.com>
2023-11-27 00:26:36 +0530

    updated landing page navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -44,7 +44,7 @@ const Nav = (props) => {
       </ul>
       
         {/* Desktop Navigation */}
-      <div className='sm:flex hidden'>
+      <div className='sm:flex flex'>
         
           <>
             {providers &&
@@ -54,7 +54,7 @@ const Nav = (props) => {
                   key={provider.name}
                   onClick={() => {
                     signIn(provider.id,{callbackUrl: '/dashboard'});
-                    console.log(session)
+                    // console.log(session)
                   }}
                   className='black_btn'
                 >
@@ -64,68 +64,6 @@ const Nav = (props) => {
           </>
        
       </div>
-
-      {/* Mobile Navigation */}
-      <div className='sm:hidden flex relative'>
-        {session?.user ? (
-          <div className='flex'>
-            <Image
-              src={session?.user.image}
-              width={37}
-              height={37}
-              className='rounded-full'
-              alt='profile'
-              onClick={() => setToggleDropdown(!toggleDropdown)}
-            />
-
-            {toggleDropdown && (
-              <div className='dropdown'>
-                <Link
-                  href='/profile'
-                  className='dropdown_link'
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  My Profile
-                </Link>
-                <Link
-                  href='/create-prompt'
-                  className='dropdown_link'
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Create Prompt
-                </Link>
-                <button
-                  type='button'
-                  onClick={() => {
-                    setToggleDropdown(false);
-                    signOut();
-                  }}
-                  className='mt-5 w-full black_btn'
-                >
-                  Sign Out
-                </button>
-              </div>
-            )}
-          </div>
-        ) : (
-          <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type='button'
-                  key={provider.name}
-                  onClick={() => {
-                    signIn(provider.id);
-                  }}
-                  className='black_btn'
-                >
-                  Sign in
-                </button>
-              ))}
-          </>
-        )}
-      </div>
-      
     </nav>
   );
 };
